e8be021ac02e77575af9adb008730f1f821cd1b6
ryu1002 <ddong9124@gmail.com>
2025-09-19 14:45:32 +0900

    JSP 시작

---
+++ src/main/java/org/example/board/DAO/BoardDAOImpl.java
@@ -0,0 +1,40 @@
+package org.example.board.DAO;
+
+import org.example.board.DTO.BoardDTO;
+
+import java.util.List;
+import java.util.Map;
+
+public class BoardDAOImpl implements BoardDAO{
+
+
+    @Override
+    public void insertData(BoardDTO dto) {
+
+    }
+
+    @Override
+    public void updateData(BoardDTO dto) {
+
+    }
+
+    @Override
+    public void deleteData(BoardDTO dto) {
+
+    }
+
+    @Override
+    public BoardDTO getReadData(int num) {
+        return null;
+    }
+
+    @Override
+    public List<BoardDTO> getList(Map<String, Object> params) {
+        return List.of();
+    }
+
+    @Override
+    public void updateViews(int num) {
+
+    }
+}
